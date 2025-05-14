b3ca4b6a1a9f3c5f9008507fea996dc8966eaded
mbrgr <max.j.ber96@gmail.com>
2025-05-14 15:13:44 +0200

    Initiale version

--- mean/Figure_2_4_optimal_bw_derivative.R
+++ mean/Figure_2_4_optimal_bw_derivative.R
@@ -9,12 +9,13 @@ set.seed(264)
 N = 1000
 n = 600
 
-p = c(65, 115, 175, 275, 400,550)                             # setting in dis
+p = c(65, 115, 175, 275, 400, 550, 750, 1000)  # setting in dis
 sigma = 1                                                          
 m = length(p)
 H = sapply(p, function(x)(rev(seq(0.3, 4/x, -0.005))))
+H[[7]] = H[[7]][1:27] # not included in Figure 2.4
+H[[8]] = H[[8]][1:28] # not included in Figure 2.4
 H
-
 
 ###### Degree = 2 ######
 alpha = 2.1
@@ -36,12 +37,12 @@ erg_deg_2_df = data.frame(sup.err = unlist(erg_deg2), p =
 
 # Local quadratic estimator bandwidth comparison 
 
-Figure24a = ggplot(erg_deg_2_df, aes(x = h, y = sup.err, color = p, pch = p)) + # [erg_deg_2_df$p %in% c(115, 175, 275, 400, 550, 1000),]
+Figure24a = erg_deg_2_df |> filter(p %in% c(65, 115, 175, 275, 400, 550)) |> 
+  ggplot(aes(x = h, y = sup.err, color = p, pch = p)) + 
   geom_point() + labs(subtitle = "n = 600") + lims(y = c(0.5, 20)) + deriv_est_theme
 Figure24a
 
-
-# ggsave("mean/grafics/derivative_bandwidth_comparison_quad.png", device = "png", width = 5, height = 3.8, units = "in")
+ggsave("mean/grafics/derivative_bandwidth_comparison_quad.png", device = "png", width = 5, height = 3.8, units = "in")
 
 ###### Degree = 3 ######
 set.seed(264) # same seed as for degree = 2
@@ -63,13 +64,14 @@ erg_deg_3_df = data.frame(sup.err = unlist(erg_deg3), p =
 ) 
 
 # Local cubic estimator bandwidth comparison 
-Figure24b = ggplot(erg_deg_3_df, aes(x = h, y = sup.err, color = p, pch = p)) + #[erg_deg_3_df$p %in% c(115, 175, 275, 400, 550, 1000),]
+Figure24b = erg_deg_3_df |> filter(p %in% c(65, 115, 175, 275, 400, 550)) |> 
+  ggplot(aes(x = h, y = sup.err, color = p, pch = p)) + 
   geom_point() + lims(y = c(0.5, 20)) + labs(subtitle = "n = 600") + deriv_est_theme
 Figure24b 
-#ggsave("mean/grafics/derivative_bandwidth_comparison_cubic.png", device = "png", width = 5, height = 3.8, units = "in")
+ggsave("mean/grafics/derivative_bandwidth_comparison_cubic.png", device = "png", width = 5, height = 3.8, units = "in")
 
-erg_deg_2_df |> group_by(p) |> slice_min(sup.err)
+erg_deg_2_df |> group_by(p) |> slice_min(sup.err)# results for table 2.1
 erg_deg_3_df |> group_by(p) |> slice_min(sup.err)
 
-#save.image("mean/data/bandwidth_comparison_per_degree.RData")
+save.image("mean/data/bandwidth_comparison_per_degree.RData")
 
---
+++ mean/Figure_2_5_mean_error_decomposition.R
@@ -0,0 +1,68 @@
+# Includes Figure 2.5 and 2.6
+
+##### source code #####
+source("mean/functions_mean.R")
+
+##### results #####
+load("mean/data/mean_error_decomposition.RData")
+
+##### Estimator Comparison #####
+N = 1000
+alpha = 2.1
+p = c(40, 60, 80, 120, 160, 200, 260, 320, 400, 480, 560)
+deg = floor(alpha)
+n = 600 # use fixed n
+
+err.decomp = matrix(0, length(p), 4)
+err.decomp.interp = matrix(0, length(p), 4)
+err.decomp.spline = matrix(0, length(p), 4)
+
+
+
+set.seed(423)
+spar = sapply(1:length(p), function(j){spl.smth.para(n, p[j], N)})
+
+h = c(rep(.09, 3), rep(.085, 2), .08, rep(.075, 3), rep(.07, 2))
+rbind(p, h)
+
+####
+set.seed(675)
+for(i in 1:length(p)){
+  err.decomp.interp[i, ] = sampleAndDecompositionBB.interp(n, p[i], N)
+  err.decomp[i, ] = sampleAndDecompositionBB(n, p[i], h[i], N, deg = 2)
+  err.decomp.spline[i, ] = sampleAndDecompositionBB.spline(n, p[i], N, spar[i])
+}
+
+
+df.erg.locpol = data.frame(sup.error = as.vector(t(err.decomp)), 
+                           p = rep(p, each = 4), 
+                           term = rep(c("sup.err", "bias", "eps", "Z"), length(p)), method = "LocPol")
+df.erg.interp = data.frame(sup.error = as.vector(t(err.decomp.interp)), 
+                           p = rep(p, each = 4), 
+                           term = rep(c("sup.err", "bias", "eps", "Z"), length(p)), method = "Interp")
+df.erg.spline = data.frame(sup.error = as.vector(t(err.decomp.spline)), 
+                           p = rep(p, each = 4), 
+                           term = rep(c("sup.err", "bias", "eps", "Z"), length(p)), method = "Spline")
+
+
+df.erg = rbind(df.erg.locpol, df.erg.interp, df.erg.spline)
+
+# Figure 2.5
+ggplot(df.erg[df.erg$term != "sup.err",], aes(p, sup.error, lty = term, pch = method, col = term)) + 
+  geom_point() + geom_line() + labs(subtitle = "n = 600")  + 
+  lims(y = c(0, 0.16))
+
+ggsave("mean/grafics/supErr_comp_interp_lp_spline.png", device = "png",
+       width = 5, height = 3.8, units = "in")
+
+# Figure 2.6
+ggplot(df.erg[df.erg$term == "sup.err",],
+       aes(p, sup.error, pch = method, lty = method, col = method)) + 
+  geom_point() + geom_line() + 
+  labs(subtitle = "n = 600") + 
+  lims(y = c(0, 0.16)) 
+
+ggsave("supErrDecomp_comp_interp_lp_spline.pdf", device = "pdf",
+       width = 5, height = 3.8, units = "in")
+
+save.image("mean/data/mean_error_decomposition.RData")
