83d102e7d630f31a428fc00e066af5daa761cd53
mbrgr <max.j.ber96@gmail.com>
2025-05-19 14:37:47 +0200

    updated figure 3.9

--- cov/figure_3_9_illustration.R
+++ cov/figure_3_9_illustration.R
@@ -5,7 +5,8 @@ library(tictoc)
 library(plotly)
 
 #### results ####
-load("cov/data/results_figure_3_9.RData") # for Illustration in Figure 3.9
+load("cov/data/results_figure_3_9a.RData") # for Illustration in Figure 3.9a
+load("cov/data/results_figure_3_9b.RData") # for Illustration in Figure 3.9b
 # without the weights w and w3
 
 source("cov/functions.R")
@@ -21,7 +22,7 @@ p = 50
 n = 100#
 h = 0.6
 x.design = (1:p - 0.5)/p
-#set.seed(99)
+set.seed(99)
 Y = biLocPol::OU(n, x.design, sigma = sigma, alpha = theta) + matrix(rnorm(n * p, 0, sd), n, p) # n x p
 Z = observation_transformation(Y)
 w = local_polynomial_weights(p, h, p.eval, F, m = 2, grid.type = "less", del = 1)
@@ -57,14 +58,10 @@ figure39a
 save_image(figure39a, 
            file = "cov/grafics/OU_G10.pdf", 
            width = 600, height = 750)
-#plot_ly() |> 
-#  add_surface(x = x[trim], y = x[trim], z = abs(G10_UP - estimate_UP)[trim, trim]) |> 
-#  add_surface(x = x[trim], y = x[trim], z = abs(G10_DN - estimate_DN)[trim, trim]) |> 
-#  layout(scene = list(xaxis = list(title = ""), 
-#                     yaxis = list(title = ""), 
-#                    zaxis = list(title = ""))) 
 
-n 
+rm(w)
+save.image("cov/data/results_figure_3_9a.RData")
+
 # parameter second process
 
 del10_cov_z_2rv = function(x){
@@ -77,14 +74,14 @@ del01_cov_z_2rv = function(x){
 
 # data 
 n = 100
-#h2 = 0.3
-h3 = 0.2
-#w2 = local_polynomial_weights(p, h2, p.eval, F, m = 2, grid.type = "less", del = 1)
-w3 = local_polynomial_weights(p, h3, p.eval, F, m = 2, grid.type = "less", del = 1)
+h2 = 0.3
+#h3 = 0.2
+w2 = local_polynomial_weights(p, h2, p.eval, T, m = 2, grid.type = "less", del = 1)
+#w3 = local_polynomial_weights(p, h3, p.eval, F, m = 2, grid.type = "less", del = 1)
 set.seed(45)
 Y2 = biLocPol::z_2rv(n, p) + matrix(rnorm(n * p, 0, sd), n, p) # n x p
 Z2 = observation_transformation(Y2)
-estimate_2 = eval_weights(w3, Z2)[,,3]
+estimate_2 = eval_weights(w2, Z2)[,,3]
 G10_2 = matrix(apply(observation_grid(p.eval, comp = "full"), 1, del01_cov_z_2rv), p.eval, p.eval)
 
 UP = upper.tri(G10_2, diag = T)
@@ -96,11 +93,15 @@ estimate_2UP[DN] = NA
 
 ##### Figure 3.9 (b) #####
 figure39b = plot_ly() |> 
-  add_surface(x = x, y = x, z = G10_2, alpha = .8) |> 
+  add_surface(x = x, y = x, z = G10_2, alpha = .8, showscale = F) |> 
   add_surface(x = x, y = x, z = estimate_2UP, alpha = .8, colorscale = cs2, showscale = F) |> 
   add_surface(x = x, y = x, z = estimate_2DN, alpha = .8, colorscale = cs2, showscale = F) |>
   front_layout(x = -.9, y = -2.3, z = .7)
 figure39b
 
+save_image(figure39a, 
+           file = "cov/grafics/2rv_G10.pdf", 
+           width = 600, height = 750)
+
 rm(w, w3)
-save.image("cov/data/results_figure_3_9.RData")
+save.image("cov/data/results_figure_3_9b.RData")
