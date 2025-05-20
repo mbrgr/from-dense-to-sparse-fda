7dec55f8dbc79ff6897c9679e79b661a65f53753
mbrgr <max.j.ber96@gmail.com>
2025-05-20 11:09:11 +0200

    minor changes

--- weather/weather_covariance_derivatives.R
+++ weather/weather_covariance_derivatives.R
@@ -9,6 +9,8 @@ library(plotly)
 library(locpol)
 library(ffscb)
 
+#### results ####
+load("weather/data/weather_estimation.RData")
 
 
 #### Load and process the data ####
@@ -45,8 +47,7 @@ N3 = N3[-to_rm, ]
 sum(is.na(N3))
 
 #### (Co)-Variance Estimation ####
-#### results ####
-load("weather/data/weather_estimation.RData")
+
 p.eval = 144
 p = 144
 
@@ -125,21 +126,93 @@ final_est %>%
 
 ggsave("mean/grafics/weather_part_deriv_gamma_diagonal_all_months.pdf", device = "pdf", width = 9, height = 6, units = "in")
 
-##### Figure 3.13 #####
-final_est %>% 
-  filter(name == 4 ) %>% 
-  ggplot(aes( x = time, y = value, color = g, linetype = g)) + 
+#### 3 dimensional plots ####
+add = 44
+p.eval = 100
+p = 144 + 2*add
+x.design = (0:(144-1))/144
+x.design.extended = c( x.design[ (144-add+1):144 ] - 1, x.design, x.design[1:add] + 1)
+
+W = local_polynomial_weights(p, 0.3, p.eval, T, m = 2, del = 1, 
+                             eval.type = "full", x.design.grid = x.design.extended) # watch out for evaluation
+
+g_hat = array(0, dim = c(p.eval, p.eval, 12))
+for ( m in 1:12) {
+  Y = N3[N3[,2] == m, -(1:3)] |>  
+    observation_transformation()
+  g_hat[,,m] = eval_weights(W, Y)[,,2]
+}
+
+
+time = (1:p.eval - 0.5)/p.eval * 24
+
+
+cs2 = lisG10_UPcs2 = list(c(0, 1), c("lightblue", "darkred"))
+UP = upper.tri(g_hat[,,3], diag = T)
+estimate_03up = g_hat[,,3]
+estimate_03dn = g_hat[,,3]
+estimate_03dn[UP] = NA
+estimate_03up[!UP] = NA
+plot_ly() %>% 
+  add_surface(x = ~ time, y = ~ time, z = estimate_03up, alpha = 0.9, colorscale = cs2, lighting = list(
+    ambient = 0.7, diffuse = 0.8, specular = 0.1, roughness = 0.9
+  )) %>% 
+  add_surface(x = ~ time, y = ~ time, z = estimate_03dn, alpha = 0.9, colorscale = cs2, lighting = list(
+    ambient = 0.7, diffuse = 0.8, specular = 0.1, roughness = 0.9
+  )) %>% 
+  layout(
+    scene = list(
+      xaxis = list(title = "time"),
+      yaxis = list(title = "time"),
+      zaxis = list(title = "")
+    )
+  )%>%
+  layout(
+    scene = list(
+      aspectratio = list(x = 1, y = 1, z = 1)  # Controls axis scaling
+    )
+  )
+# save: cov01_march.png
+
+UP = upper.tri(g_hat[,,11], diag = T)
+estimate_11up = g_hat[,,11]
+estimate_11dn = g_hat[,,11]
+estimate_11dn[UP] = NA
+estimate_11up[!UP] = NA
+plot_ly() %>% 
+  add_surface(x = ~ time, y = ~ time, z = estimate_11up, alpha = 0.9, lighting = list(
+    ambient = 0.7, diffuse = 0.8, specular = 0.1, roughness = 0.9
+  )) %>% 
+  add_surface(x = ~ time, y = ~ time, z = estimate_11dn, alpha = 0.9, lighting = list(
+    ambient = 0.7, diffuse = 0.8, specular = 0.1, roughness = 0.9
+  )) %>% 
+  layout(
+    scene = list(
+      xaxis = list(title = "time"),
+      yaxis = list(title = "time"),
+      zaxis = list(title = "")
+    )
+  )  %>%
+  layout(
+    scene = list(
+      aspectratio = list(x = 1, y = 1, z = 1)  # Controls axis scaling
+    )
+  )
+
+
+#save: cov01_nov.png
+
+deriv_cov_weather_diag = tibble(x = rep(time, 2), 
+                                est = c(diag(g_hat[,,2]), diag(g_hat[,,3])), 
+                                deriv = gl(2, 72, labels = c("G10", "G01")))
+deriv_cov_weather_diag |> 
+  ggplot(aes( x = x, y = est, color = deriv, linetype = deriv)) + 
   geom_line(linewidth = .9) +
   deriv_est_theme + 
   labs(y = NULL, x = "time", title = expression(Partial~derivatives~of~Gamma~on~the~diagonal) ) + 
   scale_discrete_manual(
     aesthetics = c("color", "linetype"),
-    values = c("G10" = 2,"G01" = 4), 
+    values = c(2,4), 
     name = "Deriv.",
-    labels = c("G10" = expression(italic(d)^{"(1,0)"}*Gamma), "G01" = expression(italic(d)^{"(0,1)"}*Gamma))
-  )  + 
-  scale_x_continuous(breaks = c(time[25], time[73], time[121], time[169], time[217]))
-
-ggsave("mean/grafics/weather_part_deriv_gamma_diagonal.pdf", device = "pdf", width = 5, height = 3.8, units = "in")
-
-#save.image("data/weather_covariance_derivative_estimation.RData")
+    labels = c(expression(italic(d)^{"(1,0)"}*Gamma), expression(italic(d)^{"(0,1)"}*Gamma))
+  ) 
